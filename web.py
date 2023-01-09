c2d32d67a260c06df3e60f3acd34d658a219568e
EricLod <eric.wiehman@lodicon.co.za>
2023-01-09 11:12:03 +0200

    Implement remove todo

--- web.py
+++ web.py
@@ -2,8 +2,8 @@ import streamlit as st
 from modules import functions
 
 def add_todo():
-    todo = st.session_state["new_todo"]
-    todos.append(todo + "\n")
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
     functions.write_todos(todos)
 
 todos = functions.get_todos()
@@ -12,8 +12,13 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="",
               placeholder="Enter new todo...",
